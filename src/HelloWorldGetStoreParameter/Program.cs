bacfa126339be83ec70180b4456a28a259d8d6e7
wodson <wodsonluiz@live.com>
2024-10-08 22:14:30 -0300

    feat: Add project

---
+++ src/HelloWorldGetStoreParameter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Amazon;
+using Amazon.SimpleSystemsManagement;
+using Amazon.SimpleSystemsManagement.Model;
+
+namespace HelloWorldGetStoreParameter;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        var region = RegionEndpoint.USEast1;
+
+        var ssmClient = new AmazonSimpleSystemsManagementClient(region);
+
+        var parameterName = "connectionstring-dev";
+
+        var request = new GetParameterRequest
+        {
+            Name = parameterName,
+            WithDecryption = false
+        };
+
+        var response = ssmClient.GetParameterAsync(request)
+            .GetAwaiter()
+            .GetResult();
+
+        var result = response.Parameter.Value;
+        Console.WriteLine($"Value is {result}");
+    
+    }
+}
